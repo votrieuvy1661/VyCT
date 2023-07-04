5abb8a8a1b9bdeae5b1c540f2d9ab764f7783ac9
vtvy <votrieuvy1661@gmail.com>
2023-07-04 22:26:57 +0700

    Rebase git

--- src/views/NotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="page">
-        <p>
-            Oops, không thể tìm thấy trang. Trở về
-            <router-link to="/"> trang chủ. </router-link>
-        </p>
-    </div>
-</template>
